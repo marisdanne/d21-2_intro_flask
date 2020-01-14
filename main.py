92ab85f98b7415a801856a3c5db14f42f86d8c5b
marisdanne <maris.danne@rvt.lv>
2020-01-14 10:19:35 +0000

    add active links

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/index")
 @app.route('/home')
 def home():
-  return render_template('home.html')
+  return render_template('home.html', active_page = 'home')
 
 @app.route("/about")
 def getAbout():
