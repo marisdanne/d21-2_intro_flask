092724f75b9f94fb54a2d34e3e04b5265f67340f
Māris Danne <Maris.Danne@rvt.lv>
2020-02-03 15:31:07 +0200

    add post/get

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from file_proc import read_file, write_file
 
 app = Flask(__name__)
 
@@ -23,6 +24,32 @@ def params():
 @app.route('/post_req', methods = ['POST'])
 def post_req():
   return request.args
+
+@app.route('/read_file')
+def read_from_file():
+  content = read_file()
+  return content
+
+@app.route('/write_file', methods = ['POST'])
+def write_to_file():
+  if request.content_type == 'application/json':
+    contentJSON = request.get_json()
+    write_file(contentJSON['data'])
+    return f"Add line {contentJSON['data']} to file."
+  else:
+    return f"Invalid request {request.content_type} not supported!"
+
+
+@app.route('/file', methods = ['GET', 'POST'])
+def fileWork():
+  if request.method == 'GET':
+    return read_from_file()
+  elif request.method == 'POST':
+    return write_to_file()
+  else:
+    return f"Method {request.method} not supported!"
+
+
 
 if __name__ == '__main__':
   app.run(host="0.0.0.0", threaded=True, port=5000, debug=True)
